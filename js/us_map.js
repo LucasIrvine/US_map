d1ea834b740849549f6f042b8a2fa4b38aa51508
Lucas <lirvine@nI-irvine.local>
2013-10-02 16:57:42 -0500

    stuff and things

--- js/us_map.js
+++ js/us_map.js
@@ -1,4 +1,4 @@
-USmap = {
+var USmap = {
   
   width: 960,
   height: 500,
@@ -34,10 +34,9 @@ USmap = {
   
   grabJson: function(){
     d3.json('data/us.json', function (error, us) {
-      
       //self.drawBackground(us);
       self.drawGroup(us);
-      self.drawCounties(us);
+     
     });
   },
 
@@ -53,19 +52,22 @@ USmap = {
 
   drawGroup: function(us){
     self.g = self.svg.append('g');
-
+    self.drawCounties(us);
     self.drawStates(us);
   },
 
   drawStates: function(us){
     self.g.append('g')
-      .attr('id', 'states')
+      .attr({'id' : 'states'})
       .selectAll('path.state')
       .data(topojson.feature(us, us.objects.states).features)
       .enter()
         .append('path')
         .attr('d', self.path)
-        .attr('class', 'state')
+        .attr({
+          'class' : 'state',
+          'opacity' : 0
+        })
         .on('click', function(d,i){
           self.stateClick(d);
         });
@@ -95,8 +97,19 @@ USmap = {
     .classed('active', self.centered && function(d) { return d === self.centered; });
 
     self.g.transition()
-      .duration(750)
+      .duration(600)
       .attr("transform", "translate(" + self.width / 2 + "," + self.height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")");
+  },
+
+  drawCounties: function(us){
+    self.g.append('g')
+      .attr('id', 'counties')
+      .selectAll('path.county')
+      .data(topojson.feature(us, us.objects.counties).features)
+       .enter()
+        .append('path')
+        .attr('class', 'county')
+        .attr('d', self.path);
   },
 
   zoomOutBtn: function(){
@@ -126,7 +139,6 @@ USmap = {
         'x' : 65,
         'y' : 78
       });
-    
   },
 
   zoomBtnRemove: function(){
@@ -135,7 +147,6 @@ USmap = {
 
      d3.select('.zoomBtnText')
       .remove();
-
   },
 
   zoomOut: function(){
@@ -158,24 +169,8 @@ USmap = {
       .transition()
       .style('opacity', 0)
       .remove();
+  }
 
-
-  },
-  
-  drawCounties: function(us){
-    self.g.append('g')
-      .attr('id', 'counties')
-      .selectAll('path.county')
-      .data(topojson.feature(us, us.objects.counties).features)
-       .enter()
-        .append('path')
-        .attr('class', 'county')
-        .attr('d', self.path);
-  },
-
-  countyClick: function(county, us){
-    console.log(county, us);
-  },
 };
 
 
